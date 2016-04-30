ae876c4cf3b84fe341125b86137d559ad86f8687
mrujowfi <mrijowfi@gmail.com>
2016-04-30 21:37:48 +0800

    share_line5
    share_line5

---
+++ module_share.py
@@ -0,0 +1,110 @@
+#coding=utf-8
+__author__ = 'lw'
+'''
+http://summary.jrj.com.cn/hybk/index.shtml
+概念板块ajax数据:http://q.jrjimg.cn/?q=cn|bk|5&n=hqa&c=l&o=pl,d&p=1050&_dc=1437575957282
+行业板块ajax数据:http://q.jrjimg.cn/?q=cn|bk|7&n=hqa&c=l&o=pl,d&p=1050&_dc=1437576371462
+地域板块ajax数据:http://q.jrjimg.cn/?q=cn|bk|3&n=hqa&c=l&o=pl,d&p=1050&_dc=1437576482336
+证监会行业ajax数据:http://q.jrjimg.cn/?q=cn|bk|1&n=hqa&c=l&o=pl,d&p=1020&_dc=1437576515412
+全球行业ajax数据:ttp://q.jrjimg.cn/?q=cn|bk|2&n=hqa&c=l&o=pl,d&p=1020&_dc=1437576648602
+股票所属类别有交叉, 用逗号分开
+某板块ajax数据:http://q.jrjimg.cn/?q=cn|s|bk400115932&c=m&n=hqa&o=pl,d&p=1050&_dc=1437740704114
+'''
+import requests
+import time
+import json
+import os
+import urllib
+import datetime
+import traceback
+import pprint
+
+global allShares, dictStart, getShares, dictPlate
+
+
+# start_date & end_date 's format: yyyy-mm-dd
+def get_historical_prices(symbol, start_date, end_date, proxies=None):
+    params = urllib.urlencode({
+        's': symbol,
+        'a': int(start_date[5:7]) - 1,
+        'b': int(start_date[8:10]),
+        'c': int(start_date[0:4]),
+        'd': int(end_date[5:7]) - 1,
+        'e': int(end_date[8:10]),
+        'f': int(end_date[0:4]),
+        'g': 'd',
+        'ignore': '.csv',
+    })
+    url = 'http://ichart.yahoo.com/table.csv?%s' % params
+    # proxies = {
+    #     "http": "http://123.58.129.48:443"
+    # }
+    req = requests.get(url, timeout=15, proxies=proxies)
+    content = req.text
+    daily_data = content.splitlines()
+    hist_dict = dict()
+    try:
+        keys = daily_data[0].split(',')
+        for day in daily_data[1:]:
+            day_data = day.split(',')
+            date = day_data[0]
+            hist_dict[date] = \
+                {keys[1]: day_data[1],
+                 keys[2]: day_data[2],
+                 keys[3]: day_data[3],
+                 keys[4]: day_data[4],
+                 keys[5]: day_data[5],
+                 keys[6]: day_data[6]}
+    except IndexError, e:
+        print str(e)
+        print content
+        hist_dict = None
+    return hist_dict
+
+'''
+先从股票列表里把所有股票查出来, 然后从2010到2015年都下下来保存, 记得要以股票名为文件名
+如果失败就重来, 检测没有同名数据就从雅虎下载, 直到所有遍历
+然后再遍历文件, 把每一天的均价扫进去, 计算涨跌, 保存到数据库, 再删除文件
+出错则重新遍历文件, 直到没有文件.
+'''
+
+
+def synHistory(shares, date1='2015-01-01', date2='today'):
+    listCode = list()
+    for each in shares:
+        listCode.append(each)
+    print len(listCode)
+    if date2 == 'today':
+        startDate = date1
+        endDate = datetime.datetime.now().strftime("%Y-%m-%d")
+    else:
+        startDate = date1
+        endDate = date2
+    # proxies = {"http": "http://112.74.86.238:3128"}
+    proxies = None
+    while True:
+        try:
+            for i in range(len(listCode)):
+                each = listCode[i]
+                print str(i)+'/'+str(len(listCode))
+                if os.path.exists(os.path.dirname(__file__)+'/data_share/'+each+'.txt'):
+                    os.remove(os.path.dirname(__file__)+'/data_share/'+each+'.txt')
+                data = get_historical_prices(each, startDate, endDate, proxies)
+                f = open(os.path.dirname(__file__)+'/data_share/'+each+'.txt', 'w')
+                f.write(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
+                f.close()
+        except Exception, e:
+            print 'step2 error:'+str(e)
+            stack = traceback.format_exc()
+            print stack
+            if str(e).__contains__('10054') or str(e).__contains__('index out of'):
+                print 'change proxies'
+            time.sleep(5)
+            continue
+        break
+
+
+if __name__ == '__main__':
+    print os.path.dirname(__file__)
+    synHistory(['600229.SS'])
+
---
+++ module_share_kelly.py
@@ -0,0 +1,140 @@
+# coding=utf-8
+__author__ = 'lw'
+
+import json
+import os
+
+# f = (bp - cq) / (bc)
+
+global share_data, except_data, money, cash, gu, sum_day, history, last_f
+share_data = {}
+except_data = {}
+money = 100000.0
+cash = 100000.0
+gu = 0
+sum_day = 0
+history = 120
+last_f = 0.0
+
+
+def load_share(txt_name):
+    global share_data, except_data, money, cash, gu, sum_day, history, last_f
+
+    share_data = {}
+    except_data = {}
+    money = 10000.0
+    cash = 10000.0
+    gu = 0
+    sum_day = 0
+    history = 120
+    fin = open(os.path.dirname(__file__)+'/data_share/'+txt_name+'.txt')
+    share_data = json.load(fin)
+    fin.close()
+    #print len(share_data)
+
+    except_data = {}
+    return count_f3(txt_name)
+
+# ok
+def count_f3(txt_name):
+    global share_data, except_data, money, cash, gu, sum_day, history, last_f
+    # b = max_last_week/last_close - 1
+    # c = 1 - min_last_week/last_close
+
+    if share_data is None:
+        return
+    keys = share_data.keys()
+    keys.sort()
+    keys = keys[-250:]
+    #print len(keys)
+
+    if len(keys) > 5:
+        for i in range(history, len(keys)):
+            max_last = 0
+            min_last = 1000
+            last_close = float(share_data[keys[i-1]]['Close'])
+            up_percent = 0
+            down_percent = 0
+            for j in range(i-history, i):
+                max_last = float(share_data[keys[j]]['Close']) if float(share_data[keys[j]]['Close']) > max_last else max_last
+                min_last = float(share_data[keys[j]]['Close']) if float(share_data[keys[j]]['Close']) < min_last else min_last
+            for j in range(i-5, i):
+                if float(share_data[keys[j]]['Close']) >= float(share_data[keys[j]]['Open']):
+                    up_percent += 1
+                if float(share_data[keys[j]]['Close']) < float(share_data[keys[j]]['Open']):
+                    down_percent += 1
+            p = float(up_percent)/history
+            q = float(down_percent)/history
+            b = max_last/last_close - 1
+            c = 1 - min_last/last_close
+            # print keys[i],
+            #print keys[i], b, c
+            #print '\t', max_last_week, min_last_week, last_close
+            if b*c != 0:
+                f = (p*b - q*c)/(b*c)
+            else:
+                f = 0.5
+            f = round(f, 1)
+            # print f, max_last, min_last
+
+            if f > 1:
+                f = 1
+            if f < 0:
+                f = 0
+            if last_close > max_last:
+                f = 1
+            if last_close < min_last:
+                f = 0
+            # print f
+
+            trade(last_close, f)
+
+    return summary(txt_name)
+
+def trade(last_close, f):
+    global share_data, except_data, money, cash, gu, sum_day, history, last_f
+
+    money = cash + gu*last_close
+    all_in = money*f
+    if all_in > gu*last_close:
+        # 买入
+        gu_in = int((all_in - gu*last_close)/last_close)
+        cash -= gu_in*last_close
+        gu += gu_in
+    elif all_in < gu*last_close:
+        # 卖出
+        gu_out = int((gu*last_close - all_in)/last_close)
+        cash += gu_out*last_close
+        gu -= gu_out
+    money = cash + gu*last_close
+
+    sum_day += 1
+    last_f = f
+
+
+def summary(txt_name):
+    global share_data, except_data, money, cash, gu, sum_day, history, last_f
+
+    print money
+    return (str(money), str(last_f), str(sum_day))
+
+if __name__ == '__main__':
+    # file_name = os.listdir(r'G:\llw\code\python\shares\step')
+    # result = {}
+    # for i in range(len(file_name)):
+    #     each = file_name[i]
+    #     load_share(each)
+    #     count_f3()
+    #     print str(i)+'/'+str(len(file_name)),
+    #     end = summary()
+    #     result[each] = end
+    # r_keys = result.keys()
+    # r_keys.sort()
+    # for each in r_keys:
+    #     print ','.join(result[each])+','+each
+    # print 'go'
+    end = load_share('600229.SS')
+    print end[1]
+
+
+
